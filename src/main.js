462ec308ab388a0a05fa752f4538eadb20466833
Zooakva <zooakva2@gmail.com>
2024-04-07 20:30:17 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,21 +11,19 @@ let query = '';
 const displayLoader = () => {
   galleryList.innerHTML += '<span class="loader"></span>';
 };
+const searchForm = document.querySelector('.search-bar');
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
 
-const inputQuery = document.getElementById('search-input');
-inputQuery.addEventListener('input', e => {
-  query = inputQuery.value.trim();
-});
+  const query = document.getElementById('search-input').value.trim();
 
-const searchButton = document.getElementById('search-button');
-searchButton.addEventListener('click', () => {
   if (query && query.length >= 3) {
     displayLoader();
 
     fetchImages(query)
       .then(data => {
         renderImages(data);
-        inputQuery.value = '';
+        document.getElementById('search-input').value = '';
       })
       .catch(error => {
         iziToast.error({
@@ -33,7 +31,7 @@ searchButton.addEventListener('click', () => {
           message: `Sorry, there are no images matching your search query. Please try again!`,
           position: 'topRight',
         });
-        inputQuery.value = '';
+        document.getElementById('search-input').value = '';
       });
   } else {
     iziToast.info({
@@ -41,12 +39,5 @@ searchButton.addEventListener('click', () => {
       message: 'Please enter a valid search query with at least 3 characters.',
       position: 'topRight',
     });
-  }
-});
-
-inputQuery.addEventListener('keypress', e => {
-  if (e.key === 'Enter') {
-    e.preventDefault();
-    searchButton.click();
   }
 });
